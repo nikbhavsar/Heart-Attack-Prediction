1b30574ec1158cbe18ef94763f8d28725671dc4b
nikbhavsar <bhavsarnikhar21@gmail.com>
2025-06-07 09:32:15 -0700

    Rereference utilities

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 from flask_cors import CORS
 import sys
 import os
-sys.path.append(os.path.abspath('../utilities'))
+sys.path.append(os.path.abspath('./utilities'))
 import global_utils
 import pre_processing_utils
 import model_training_utils
